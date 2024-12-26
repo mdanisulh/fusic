c647ed0eac6d1014506db05d2dd31c9029d4ca22
Md Anisul Haque <mdanisulh10@gmail.com>
2024-12-26 23:24:36 +0530

    fix: Update selectors in content script for correct element targeting

--- extension/content.js
+++ extension/content.js
@@ -3,7 +3,7 @@
   const n = parseInt(
     document
       .querySelector(
-        "#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw > div.main-view-container > div.main-view-container__scroll-node > div:nth-child(2) > div > main > section > div.NXiYChVp4Oydfxd7rT5r.NxEINIJHGytq4gF1r2N1.or84FBarW2zQhXfB9VFb.odS2IW9wfNVHhkhc0l_X.XNjgtSbyhshr7YQcVvry.O0AN8Ty_Cxd4iLwyKATB.RMDSGDMFrx8eXHpFphqG > div.iWTIFTzhRZT0rCD0_gOK.contentSpacing > div.RP2rRchy4i8TIp1CTmb7 > div > span",
+        "#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw > div > div.main-view-container__scroll-node.main-view-container__scroll-node--offset-topbar > div:nth-child(2) > div > main > section > div.NXiYChVp4Oydfxd7rT5r.NxEINIJHGytq4gF1r2N1.or84FBarW2zQhXfB9VFb.odS2IW9wfNVHhkhc0l_X.XNjgtSbyhshr7YQcVvry.O0AN8Ty_Cxd4iLwyKATB.JYKKZFIXuf9lIHVeszuS > div.iWTIFTzhRZT0rCD0_gOK.contentSpacing > div.RP2rRchy4i8TIp1CTmb7 > div > div.GI8QLntnaSCh2ONX_y2c > span:nth-child(1)",
       )
       ?.innerText.split(" ")[0] ?? 0,
   );
@@ -11,7 +11,7 @@
     return;
   }
   const name = document.querySelector(
-    "#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw > div.main-view-container > div.main-view-container__scroll-node > div:nth-child(2) > div > main > section > div.NXiYChVp4Oydfxd7rT5r.NxEINIJHGytq4gF1r2N1.or84FBarW2zQhXfB9VFb.odS2IW9wfNVHhkhc0l_X.XNjgtSbyhshr7YQcVvry.O0AN8Ty_Cxd4iLwyKATB.RMDSGDMFrx8eXHpFphqG > div.iWTIFTzhRZT0rCD0_gOK.contentSpacing > div.RP2rRchy4i8TIp1CTmb7 > span.rEN7ncpaUeSGL9z0NGQR",
+    "#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw > div > div.main-view-container__scroll-node.main-view-container__scroll-node--offset-topbar > div:nth-child(2) > div > main > section > div.NXiYChVp4Oydfxd7rT5r.NxEINIJHGytq4gF1r2N1.or84FBarW2zQhXfB9VFb.odS2IW9wfNVHhkhc0l_X.XNjgtSbyhshr7YQcVvry.O0AN8Ty_Cxd4iLwyKATB.JYKKZFIXuf9lIHVeszuS > div.iWTIFTzhRZT0rCD0_gOK.contentSpacing > div.RP2rRchy4i8TIp1CTmb7 > span.rEN7ncpaUeSGL9z0NGQR",
   )?.innerText;
 
   for (let i = 2; i <= n + 1; i++) {
